09dd8a53be0928a15872ba68e9fd2618361144e1
jlangkau <jlangkau@nicos-rd.com>
2020-08-14 14:22:07 +0200

    work on auth, here sessions on amec

--- src/agent.amec.js
+++ src/agent.amec.js
@@ -6,12 +6,37 @@ module.exports = ({
 
     auth_modules = ((Array.isArray(auth_modules)) ? auth_modules : [auth_modules]);
 
+    const
+        sessions    = new Map()
+    ;
     let
         i           = 0,
         single_mech = undefined,
         _mech       = {}
     ; // let
 
+    //region fn
+
+    function toSession(auth) {
+        let
+            key  = `urn:auth:${auth['mech']}:${auth['identity']['@id']}`,
+            node = sessions.get(key)
+        ;
+        if (node) {
+            node['auth']['requestedAt'] = ((new Date).valueOf() / 1000);
+            node['bestbefore']          = (node['auth']['requestedAt'] + /** seconds */ 300);
+        } else {
+            node = {
+                'auth':       auth,
+                // REM: something like time:Instant (here: numericTimePosition)
+                'bestbefore': (((new Date).valueOf() / 1000) + /** seconds */ 300)
+            };
+            sessions.set(key, node);
+        } // if ()
+        return node['auth'];
+    } // function toSession()
+
+    //endregion fn
 
     function agent_amec(request, mech = undefined, timeout = default_timeout) {
 
@@ -29,14 +54,15 @@ module.exports = ({
                 if (mech) {
                     _mech[mech](request).then((auth) => {
                         clearTimeout(semaphore);
-                        resolve(auth);
+                        resolve(toSession(auth));
                     }).catch((err) => {
                         clearTimeout(semaphore);
                         reject(err);
                     }); // _mech[mech]()
                 } else {
-                    Promise.race(auth_modules.map((am) => am(request))).then((auth) => {
+                    Promise.race(auth_modules.map((am) => am(request, auth_from_session))).then((auth) => {
                         clearTimeout(semaphore);
+                        toSession(auth);
                         resolve(auth);
                     }).catch((err) => {
                         clearTimeout(semaphore);
