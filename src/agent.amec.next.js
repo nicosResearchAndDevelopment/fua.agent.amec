0984baf8972d5a1f392c236cef7ae22742e13f52
jlangkau <jlangkau@nicos-rd.com>
2021-09-10 17:21:47 +0200

    work

--- src/agent.amec.next.js
+++ src/agent.amec.next.js
@@ -14,20 +14,21 @@ exports.authMechanism = function (mechanism, callback) {
     authModules.set(mechanism, callback);
 };
 
-exports.authenticate = async function (request, mechanism) {
+exports.authenticate = async function (request, mechanism, users) {
     assert(util.isString(mechanism), 'authenticate : expected mechanism to be a string', TypeError);
     assert(authModules.has(mechanism), 'authenticate : auth "' + mechanism + '" is not defined');
-    assert(util.isObject(request) && util.isString(request.url), 'authenticate : expected request to be a request object', TypeError);
-    assert(util.isObject(request.session), 'authenticate : expected request to have a session', TypeError);
+    //assert(util.isObject(request) && util.isString(request.url), 'authenticate : expected request to be a request object', TypeError);
+    //assert(util.isObject(request.session), 'authenticate : expected request to have a session', TypeError);
 
     const
         callback = authModules.get(mechanism),
-        auth     = await callback(request);
+        auth     = await callback(request, users)
+    ;
 
-    if (auth) await new Promise((resolve, reject) => {
-        request.session.auth = auth;
-        request.session.save(err => err ? reject(err) : resolve())
-    });
+    //if (auth) await new Promise((resolve, reject) => {
+    //    request.session.auth = auth;
+    //    request.session.save(err => err ? reject(err) : resolve())
+    //});
 
     return auth;
 };
