4c69ea22994714f0436ec8f09c064d312cd5e851
Simon Petrac <pfoerdie@gmail.com>
2021-10-12 15:14:13 +0200

    updated BasicAuth to use domain instead of space

--- src/mechanisms/BasicAuth.js
+++ src/mechanisms/BasicAuth.js
@@ -1,50 +1,9 @@
-const
-    util = require('../agent.amec.util.js');
-
 // SEE https://datatracker.ietf.org/doc/html/rfc7617
-// NOTE this is currently only an example !!!
 function BasicAuth(config) {
-    util.assert(config.space, 'BasicAuth : expected space');
+    const domain = config.domain;
+    if (!domain) throw new Error('BasicAuth : expected domain');
 
-    // get the space and create a users map for faster access
-    const
-        space          = config.space,
-        users          = new Map(),
-        refreshTimeout = 60 * 60;
-
-    /**
-     * This method fetches the users from the space and puts them into a map.
-     * @returns {Promise<void>}
-     * @private
-     */
-    async function _refreshUsers() {
-        try {
-            const
-                /** @type {import("@nrd/fua.module.persistence").Dataset} */
-                userData  = await space.readData(undefined, 'rdf:type', 'domain:UserBasicAuthentication'),
-                userNodes = Array.from(userData.subjects()).map(namedNode => space.getNode(namedNode.value));
-            await Promise.all(userNodes.map(node => node.read()));
-            users.clear();
-            for (let userNode of userNodes) {
-                const
-                    userName     = userNode['domain:name']?.[0],
-                    userPassword = userNode['domain:password']?.[0],
-                    userActive   = userNode['domain:active']?.[0];
-                if (userName?.['@value'] && userPassword?.['@value'] && userActive?.['@value'] === 'true') {
-                    users.set(userName['@value'], userNode);
-                }
-            }
-        } catch (err) {
-            console.error(err);
-        }
-    } // _refreshUsers
-
-    // refreshing users with data from the space is asynchronous
-    _refreshUsers()
-        .then(() => setInterval(_refreshUsers, 1e3 * refreshTimeout))
-        .catch(console.error);
-
-    function basicAuth(headers) {
+    async function basicAuth(headers) {
         // get the authorization header field
         const basicAuth = headers['authorization'];
         // check if the authorization is basic
@@ -52,14 +11,14 @@ function BasicAuth(config) {
         // extract username and password
         const [name, password] = Buffer.from(basicAuth.substr(6), 'base64').toString().split(':');
         if (!(name && password)) return;
-        // get the user node from the pre fetched map
-        const userNode = users.get(name);
+        // get the user node from the domain
+        const userNode = await domain.users.getByAttr('dom:name', name);
         if (!userNode) return;
         // get the real user and password
         const auth = {
             id:       userNode['@id'],
-            user:     userNode['domain:name'][0]['@value'],
-            password: userNode['domain:password'][0]['@value']
+            user:     userNode['dom:name']?.[0]?.['@value'],
+            password: userNode['dom:password']?.[0]?.['@value']
         };
         // TODO sha256 hashing of password or something like that
         // compare the password with the real password
